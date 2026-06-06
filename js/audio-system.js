eb9ade5021f30b1edaef0fb76f7b8bca1e5d94f8
Grizzy <grizzyxixi@email.com>
2026-06-06 16:31:36 +0700

    update

--- js/audio-system.js
+++ js/audio-system.js
@@ -14,8 +14,8 @@
 const playlist = [
   {
     id:        'song1',
-    title:     'Sampai Jadi Debu',
-    artist:    'Banda Neira',
+    title:     'Cant Help Falling In Love',
+    artist:    'Elvis',
     file:      'music/song1.mp3',
     cover:     'assets/music-cover/song1.jpeg',
     startTime: 0,       // seconds — where playback begins
